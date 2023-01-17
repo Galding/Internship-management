0d9a88659d929515b008cb7547c3ec002fc0a240
Galding <nathan.peyronnet@gmail.com>
2023-01-17 14:17:28 +0100

    Finish columns filter and fix problems

--- src/main/resources/static/javascript/datatable_entreprise.js
+++ src/main/resources/static/javascript/datatable_entreprise.js
@@ -4,24 +4,40 @@ $(document).ready( function () {
             "url": "https://cdn.datatables.net/plug-ins/1.13.1/i18n/fr-FR.json",
             "search": "Recherche rapide :",
         },
-        ajax : '/entreprise/data',
+        ajax: '/entreprise/data',
         serverSide: false,
         scrollX: true,
         dom: 'Bfrtip',
-        buttons : [
+        buttons: [
             'colvis'
         ],
         order: [1, 'asc'],
-        columns : [
+        columns: [
             {
-                data: 'raisonSociale'
+              data: null,
+              render: function (data, type, row) {
+               return `<div class="bouton_operation">
+                            <a class="icon_voir" href="/entreprise/voirEntreprise?id=${row.numEntreprise}"></a>&nbsp;
+                            <a class="icon_inscrire" href="#"></a>&nbsp;
+                            <a class="icon_modifier" href="/entreprise/modifierEntreprise?id=${row.numEntreprise}"></a>&nbsp;
+                            <a class="icon_supprimer" href="/entreprise/supprimerEntreprise?id=${row.numEntreprise}"></a>&nbsp;
+                       </div>`
+              },
+              visible: true,
+              orderable: false,
+              searchable: false
+            },
+            {
+                data: 'raisonSociale',
+                visible: true
             },
             {
                 data: 'nomContact',
                 visible: false
             },
             {
-                data: 'nomResp'
+                data: 'nomResp',
+                visible: true
             },
             {
                 data: null,
@@ -30,15 +46,22 @@ $(document).ready( function () {
                         return `${row.rueEntreprise}<br/>${row.cpEntreprise} ${row.villeEntreprise}`
                     }
                     return row.cpEntreprise
-                }
+                },
+                visible: true
             },
             {
                 data: 'telEntreprise',
+                render: function (data, type, row){
+                  return row.telEntreprise.replace(/ /g, "&nbsp")
+                },
                 orderable: false,
                 visible: false
             },
             {
                 data: 'faxEntreprise',
+                render: function (data, type, row){
+                    return row.faxEntreprise.replace(/ /g, "&nbsp")
+                },
                 orderable: false,
                 visible: false
             },
@@ -57,31 +80,24 @@ $(document).ready( function () {
                     if (row.siteEntreprise === null || row.siteEntreprise === '') {
                         return '';
                     }
-                    return `
-                    <div class="field">
-                        <p class="control">
-                            <a class="button" href="${row.siteEntreprise}">
-                                <span class="icon is-small">
-                                    <i class="fa fa-link"></i>
-                                </span>
-                            </a>
-                        </p>
-                    </div>
-                    `
+                    return `<div class="bouton_operation"><a class="icon_website" href="${row.siteEntreprise}"></a></div>`
                 },
                 orderable: false,
                 searchable: false,
+                visible: true
             },
             {
                 data: 'niveau',
-                visible: false
+                visible: false,
+                searchable: true
             }, {
                 data: 'specialites',
                 render: function (data, type, row) {
                     return data.map(specialite => specialite.libelle).join('<br/>');
                 },
                 orderable: false,
-                searchable: false
+                searchable: true,
+                visible: true
             },
             {
                 data: 'enActivite',
@@ -93,4 +109,4 @@ $(document).ready( function () {
             }
         ]
     });
-} );
+});
